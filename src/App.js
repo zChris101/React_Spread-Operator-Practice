8c96823f728128e38c7c2fe9bd951fdea8473649
zCris101 <chrstnmusico@gmail.com>
2023-06-27 16:04:01 +0800

    Initial Code

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export default function App() {
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input type="text" />
+        <button>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          <li>A Item</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
